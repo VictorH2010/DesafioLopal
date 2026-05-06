f00c294c7ee5c6ae7825afc1ba3b1869cea7988b
I1HN <goismunhoz2030@gmail.com>
2026-05-06 10:48:38 -0300

    foram adicionados comentários ao exercício 5

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let numerosDigitados;
 let soma = 0
 let media
-let quantidadeDeNumeros = 0
+let quantidadeDeNumerosDigitados = 0
 
 while (numerosDigitados != 0) {
-numerosDigitados = Number(prompt("insira um número"));
-soma = soma + numerosDigitados
-quantidadeDeNumeros = quantidadeDeNumeros + 1
-media = soma / quantidadeDeNumeros
+// while serve basicamente como "enquanto", ou seja "enquanto" for diferente de 0 o código continuará se repetindo
+    numerosDigitados = Number(prompt("insira um número"));
+    soma = soma + numerosDigitados
+    quantidadeDeNumerosDigitados = quantidadeDeNumerosDigitados + 1
+    media = soma / quantidadeDeNumerosDigitados
 }
-alert(`A soma dos números digitados é ${soma}.\nvocê digitou um total de ${quantidadeDeNumeros} números.\na média desses números é de ${media}.`)
+alert(`A soma dos números digitados é ${soma}.\nvocê digitou um total de ${quantidadeDeNumerosDigitados.toFixed(2)} números.\na média desses números é de ${media}.`)
+// o comando \n serve para quebra de linha 
