e7034a67154d03fe59059fcb4129007f184b17da
I1HN <goismunhoz2030@gmail.com>
2026-05-06 10:52:17 -0300

    foram adicionados comentários ao exercício 4

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let escolha;
-
+// let escolha serve para guardar a escolha do usuário 
 while (escolha != 3) {
+    // while (escolha != 3) significa que enquanto a esolha do usuário não for 3 o código irá continuar se repetindo
     escolha = prompt("Menu\n1-Inserir produto.\n2-Listar produtos.\n3-Sair");
     if (escolha == 3) {
+        // "==" serve para atribuir valor para uma variável
         alert("Sistema encerrando.")
     }
 }
