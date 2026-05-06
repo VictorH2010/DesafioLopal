ef225439c4930b7aff21929ffa9ff6236a94f787
I1HN <goismunhoz2030@gmail.com>
2026-05-06 09:35:01 -0300

    foi criada a branch responsável pelo exercício 3

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-let i = 1; 
-let soma = 0;
-
-while (i <= 10){ 
-    soma = soma + i;
-    alert(soma) 
-    i = i + 1; 
-
-} 
+let mensagem = "";
+let numero;
+do {
+    numero = Number(prompt("Insira o número desejado para que seja feita a tabuada"));
+} while (isNaN(numero))
+for (let i = 1; i <= 10; i ++) {
+    mensagem = `${mensagem}${i} vezes ${numero} é igual a ${numero * i}
+`;
+    alert(mensagem);
+}
