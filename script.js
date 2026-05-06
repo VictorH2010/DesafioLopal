1f4500785b0383dd66fbb6c9df2892b6fc50427a
I1HN <goismunhoz2030@gmail.com>
2026-05-06 12:05:51 -0300

    foram adicionados comentários ao exercício 2

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let i = 1;
 let soma = 0;
 
 while (i <= 10){ 
+// while (nessa situação) serve para determinar que o código deve se repetir enquanto "i" for menor que 10
     soma = soma + i;
     alert(soma) 
     i = i + 1; 
