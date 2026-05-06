0b686e79c72f845c737f1fac18c351055a10241f
I1HN <goismunhoz2030@gmail.com>
2026-05-06 08:33:45 -0300

    foi criada a branch responsável pelo exercício 1

---
+++ script.js
@@ -0,0 +1,16 @@
+let numero = "";
+
+//let serve para declarar variável 
+// numero serve para receber o valor do "i"
+
+for (let i = 10; i >= 1; i = i - 1) {
+
+//let i serve para dar valor para a variável "numero"
+
+    numero = `${numero} ${i}`;
+
+}
+
+alert(numero);
+
+//alert serve para mostrar a mensagem na tela
