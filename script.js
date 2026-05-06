eb09300765dec624060ddb72c7edf419cf11b62c
I1HN <goismunhoz2030@gmail.com>
2026-05-06 13:01:50 -0300

    foi colocado (;) onde estava faltando no exercício 5

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ let quantidadeDeNumerosDigitados = 0
 while (numerosDigitados != 0) {
 // while serve basicamente como "enquanto", ou seja "enquanto" for diferente de 0 o código continuará se repetindo
     numerosDigitados = Number(prompt("insira um número"));
-    soma = soma + numerosDigitados
-    quantidadeDeNumerosDigitados = quantidadeDeNumerosDigitados + 1
-    media = soma / quantidadeDeNumerosDigitados
+    soma = soma + numerosDigitados;
+    quantidadeDeNumerosDigitados = quantidadeDeNumerosDigitados + 1;
+    media = soma / quantidadeDeNumerosDigitados;
 }
-alert(`A soma dos números digitados é ${soma}.\nvocê digitou um total de ${quantidadeDeNumerosDigitados.toFixed(2)} números.\na média desses números é de ${media}.`)
+alert(`A soma dos números digitados é ${soma}.\nvocê digitou um total de ${quantidadeDeNumerosDigitados.toFixed(2)} números.\na média desses números é de ${media}.`);
 // o comando \n serve para quebra de linha 
