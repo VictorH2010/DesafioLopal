3e8c478754834a8781ab475c5a12c93e25011969
I1HN <goismunhoz2030@gmail.com>
2026-05-06 10:59:00 -0300

    foram adicionados comentários ao exercício 3

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let mensagem = "";
 let numero;
+
 do {
     numero = Number(prompt("Insira o número desejado para que seja feita a tabuada"));
 } while (isNaN(numero))
-for (let i = 1; i <= 10; i ++) {
+// while (isNaN(numero)) equanto o usuário não digitar um número o comando ficará se repetindo.
+for (let i = 1; i <= 10; i++) {
+// for tem basicamente a mesma função do while porém com uma quantidade predefinida de vezes para ser executado.
+// ++ serve como se fosse "+1".
     mensagem = `${mensagem}${i} vezes ${numero} é igual a ${numero * i}
 `;
     alert(mensagem);
